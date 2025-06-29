0fa5877a725e71e0dff20aedd8f42405affd6cda
quangcntt31 <le6079909@gmail.com>
2025-06-29 23:26:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecureUserInfo.Data;
+using SecureUserInfo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Đăng ký dịch vụ custom
+builder.Services.AddScoped<EncryptionService>();
+builder.Services.AddScoped<LogService>();
+
+// Cấu hình Session
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // timeout sau 30 phút không hoạt động
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Phải đặt trước UseAuthorization
+app.UseSession();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# NHOM1_BTL_ATBM
